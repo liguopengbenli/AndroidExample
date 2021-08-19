79186823034dae5e561328754ebfdf23112bdb8b
guopeng li <guopeng.li@technicolor.com>
2021-08-19 10:56:09 +0200

    Add feature 1

--- app/src/main/java/com/lig/cicdexemple/MainActivity.kt
+++ app/src/main/java/com/lig/cicdexemple/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.lig.cicdexemple
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloBtn)
+        val textView = findViewById<TextView>(R.id.greetingTextView)
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        button.setOnClickListener {
+            textView.text = "Hello ${editText.text} "
+        }
     }
 }
